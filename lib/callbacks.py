504f9d4b5cdf94bdd18f10036dd0cc91154b5d6f
bergere_g8 <florian.datasetg8@gmail.com>
2024-10-19 16:02:38 +0200

    Callbacks cleaned

--- lib/callbacks.py
+++ lib/callbacks.py
@@ -11,12 +11,15 @@ def callback_train():
 
 def callback_save():
     """
-    Callback to set the global variable when the save button is pressed
+    Callback to set the global variable when the save button is pressed. Two casev are handled here.
+    Either the model is a Random Forest and then we can save the model and all the others features with
+    joblib this tool allows it.
+    Either the model is a neural network and then we need to use the keras model saver and joblib as well 
+    for the scaler part.
     """
     st.session_state.train= 0
     # It saves the model and the scaler
     with st.spinner("The model is currently saved on your computer..."):  
-        print(st.session_state.model_saving_config)
         if "model" in st.session_state.model_saving_config:
             dump(st.session_state.model_saving_config, st.session_state.model_path)
             st.session_state.save = 0
@@ -24,9 +27,6 @@ def callback_save():
         else:
             dump(st.session_state.model_saving_config, st.session_state.scaler_path)
             model = st.session_state.model_scaler_dict["model"]
-            print("in the callback")
-            print(model)
-            print(model.summary())
             model.save(st.session_state.model_path)
             st.success("The Neural Network model has been saved successfully and the sclaer as well")
 
@@ -44,7 +44,13 @@ def callback_test():
     st.session_state.test = 1
 
 def callback_stop_train():
+    """
+    Callback that allows the user to stop the train
+    """
     st.session_state.stop_train = 1
 
 def callback_reset_model():
+    """
+    Callback to reset the model used in the test part
+    """
     st.session_state.model_scaler_dict = dict()
